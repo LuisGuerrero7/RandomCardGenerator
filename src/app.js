bf884e8c7e0d5fb7408c0d8950cb70321a5a9bbd
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2024-07-20 05:02:29 +0000

    random card generator p-1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let listCardNumber = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let listCardSymbol = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomCardIndex(array) {
+    const randomCardI = Math.floor(Math.random() * array.length);
+    return array[randomCardI];
+  }
+
+  function generateCardNumber() {
+    return randomCardIndex(listCardNumber);
+  }
+
+  function generateCardSymbol() {
+    return randomCardIndex(listCardSymbol);
+  }
+
+  const cardNumber = generateCardNumber();
+  const cardSymbol = generateCardSymbol();
+
+  const cardNumberElement = document.getElementsByClassName("valor")[0];
+  cardNumberElement.innerHTML = cardNumber;
+
+  const cardSymbolElements = document.getElementsByClassName("pinta");
+  for (let element of cardSymbolElements) {
+    element.innerHTML = cardSymbol;
+  }
 };
